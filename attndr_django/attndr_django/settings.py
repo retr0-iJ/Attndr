4709dcfd0cf0cb1ee9e0994b29beef0c4e32879d
Clarik <clarik72@gmail.com>
2021-06-14 04:25:27 +0700

    Adding url for qr image

--- attndr_django/attndr_django/settings.py
+++ attndr_django/attndr_django/settings.py
@@ -170,6 +170,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 DJOSER = {
     'LOGIN_FIELD' : 'email',
